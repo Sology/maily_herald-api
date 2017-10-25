122b5f8d4950091bdb2b507c3c25a8fec77fe2d5
Wojciech Rawicz-Kosinski <w.rawicz.kosinski@gmail.com>
2017-10-25 10:51:39 +0200

    MailyHerald::SequenceMailingSerializer spec.

---
+++ spec/serializers/maily_herald/sequence_mailing_serializer_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe MailyHerald::SequenceMailingSerializer do
+
+  describe "serializing MailyHerald::SequenceMailing object" do
+    let!(:sequence)         { create :newsletters }
+    let!(:sequence_mailing) { sequence.mailings.first }
+
+    context "setup" do
+      it { expect(MailyHerald::SequenceMailing.count).to eq(3) }
+      it { expect(sequence_mailing).not_to be_nil }
+    end
+
+    it "should return serialized object" do
+      expect(described_class.new(sequence_mailing).as_json).to eq({
+        id:                   sequence_mailing.id,
+        sequenceId:           sequence_mailing.sequence_id,
+        conditions:           sequence_mailing.conditions,
+        from:                 sequence_mailing.from,
+        mailerName:           sequence_mailing.mailer_name,
+        name:                 sequence_mailing.name,
+        state:                sequence_mailing.state,
+        subject:              sequence_mailing.subject,
+        template:             sequence_mailing.template,
+        title:                sequence_mailing.title,
+        locked:               false,
+        absoluteDelayInDays:  sequence_mailing.absolute_delay_in_days
+      })
+    end
+  end
+
+end
